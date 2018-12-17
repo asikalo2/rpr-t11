324ce5e0b0a1647928216c6613933090b0c07cb3
asikalo2 <asikalo2@etf.unsa.ba>
2018-12-17 21:24:35 +0100

    Dodana metoda ispisiGradove u Main

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -13,9 +13,13 @@ public class Main {
         država"*/
 
     public static String ispisiGradove(){
-        String s = "";
-
-        return s;
+        ArrayList<Grad> gradovi = GeografijaDAO.getInstance().gradovi();
+        String rez = "";
+        for (Grad grad: gradovi) {
+            rez += grad.getNaziv() + "(" + grad.getDrzava().getNaziv() + ")" + " - " +
+            grad.getBrojStanovnika() + "\n";
+        }
+        return rez;
     }
 
     public static void glavniGrad(){
@@ -25,36 +29,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
-        //pristup bazi
-        String url = "jdbc:sqlite:resources/proba.db";
-        //String url = "jdbc:mysql://localhost/test";
-        String upit = "SELECT naziv, cijena FROM proizvodi WHERE racun=4";
-
-        try {
-            //Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url);
-            Statement stmt = conn.createStatement();
-            ResultSet result = stmt.executeQuery(upit);
-
-            float ukupno = 0;
-            while(result.next()) {
-                String naziv = result.getString(1);
-                float cijena = result.getFloat(2);
-                System.out.println (naziv + " " + cijena);
-                ukupno += cijena;
-            }
-            System.out.println("UKUPNO: "+ukupno);
-            conn.close();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            //} catch (ClassNotFoundException e) {
-            //    e.printStackTrace();
-        }
-
-        System.out.println("Gradovi su:\n" + ispisiGradove());
-        glavniGrad();
+        System.out.println(ispisiGradove());
     }
 
 }
