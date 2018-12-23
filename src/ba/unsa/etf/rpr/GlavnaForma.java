0c170e23665574fc8c9b863fbdf8a28ad42a60e0
asikalo2 <asikalo2@etf.unsa.ba>
2018-12-23 23:08:39 +0100

    Dodala Menije u javaFX formu.

--- src/ba/unsa/etf/rpr/GlavnaForma.java
+++ src/ba/unsa/etf/rpr/GlavnaForma.java
@@ -1,10 +1,17 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
+
+import java.util.ArrayList;
 
 
 public class GlavnaForma {
@@ -13,7 +20,9 @@ public class GlavnaForma {
     public TableView tableView;
     public TableColumn idColumn;
     public TableColumn nazivColumn;
-    public TableColumn glavniGradColumn;
+    public TableColumn brojStanovnikaColumn;
+    public TableColumn drzavaColumn;
+    public MenuItem exitMenuItem;
 
     public void handleKeyInput(KeyEvent keyEvent) {
     }
@@ -22,6 +31,7 @@ public class GlavnaForma {
     }
 
     public void exitAction(ActionEvent actionEvent) {
+        Platform.exit();
     }
 
     @FXML
@@ -31,5 +41,18 @@ public class GlavnaForma {
     }
 
     private void napuniTabelu() {
+        ObservableList<Grad> listaGradova = FXCollections.observableArrayList();
+
+        ArrayList<Grad> gradovi = GeografijaDAO.getInstance().gradovi();
+        idColumn.setCellValueFactory(new PropertyValueFactory("id"));
+        nazivColumn.setCellValueFactory(new PropertyValueFactory("naziv"));
+        brojStanovnikaColumn.setCellValueFactory(new PropertyValueFactory("brojStanovnika"));
+        drzavaColumn.setCellValueFactory(new PropertyValueFactory("drzava.getNaziv()"));
+
+        for (Grad g: gradovi) {
+            listaGradova.add(g);
+        }
+        tableView.setItems(listaGradova);
+
     }
 }
